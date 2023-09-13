0653dba8d3876dbbd4457713dc3040b9a4d07a93
Jim Angel <jimangel@users.noreply.github.com>
2023-09-12 20:47:38 -0500

    Update cluster-easy-add.sh
    test

--- scripts/cluster-easy-add.sh
+++ scripts/cluster-easy-add.sh
@@ -1,1 +1,1 @@
-# TODO* use context, create manual yaml (no argocd CLI) and then create / open PR for cluster.json (interactive menu).
+# TODO* use context, create manual yaml  (no argocd CLI) and then create / open PR for cluster.json (interactive menu).
